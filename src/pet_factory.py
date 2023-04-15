cff33c8be167f6feb7fb4d170143a9f8c885f7cd
MrKnowles55 <97849647+MrKnowles55@users.noreply.github.com>
2023-04-14 22:01:18 -0400

    Fixed pet_id formatting for multiword pet names. Currently is bad form of Betta Fish -> pet-betta_fish.

--- src/pet_factory.py
+++ src/pet_factory.py
@@ -5,7 +5,7 @@ from src.pet_data_utils.pet_data_manager import pet_data_manager
 
 def create_pet(pet_id, pet_level=1):
     if 'pet' not in pet_id:
-        pet_id = "pet-" + pet_id.lower()
+        pet_id = "pet-" + pet_id.lower().replace(" ", "_")
     pet_data = pet_data_manager.pet_dict[pet_id]
     pet_name = pet_data.get("name")
     return Pet(pet_name, pet_data.get("baseAttack"), pet_data.get("baseHealth"), pet_data.get("tier"), pet_level,
