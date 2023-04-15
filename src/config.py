ad8460b6e0793a2a833774b1146c4ab84fab59d3
MrKnowles55 <97849647+MrKnowles55@users.noreply.github.com>
2023-04-15 00:40:14 -0400

    Adding Config.py to allow easier parameter adjustment from terminal

--- src/config.py
+++ src/config.py
@@ -1,3 +1,9 @@
+import os
+import json
+
+parent_dir = os.path.abspath(os.path.join(os.getcwd(), ".."))
+config_path = os.path.join(parent_dir, "config.json")
+
 # Simulation Parameters
 SIMULATIONS_TO_RUN = 5000
 
@@ -9,3 +15,22 @@ ENEMY_TEAM_SIZE = 5  # 1 through 5
 
 # Other
 DEBUG_MODE = True
+
+
+def save_data(data, filename=config_path):
+    with open(filename, "w") as file:
+        json.dump(data, file, indent=4)
+
+
+def load_data(filename=config_path):
+    with open(filename, "r") as file:
+        return json.load(file)
+
+
+def display(data):
+    print(json.dumps(data, indent=4))
+
+
+if __name__ == "__main__":
+    config_data = load_data()
+    display(config_data)
