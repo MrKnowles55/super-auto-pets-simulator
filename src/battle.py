841c0ec1d6fbd2b907b9ab5979ada785f38170b8
MrKnowles55 <97849647+MrKnowles55@users.noreply.github.com>
2023-04-17 15:28:18 -0400

    added logging to battle.py to replace verbose prints

--- src/battle.py
+++ src/battle.py
@@ -1,4 +1,7 @@
 from pet import prioritize_pets
+import logger
+
+log = logger.setup_logger(__name__)
 
 
 def get_battle_string(team1, team2, prefix='', format_buffer=50, name_length=5):
@@ -13,15 +16,12 @@ def get_battle_string(team1, team2, prefix='', format_buffer=50, name_length=5):
     len1 = len(team1_pets_str)
     len2 = len(team2_pets_str)
     max_len = max(len1, len2)
-    # if max_len < format_buffer:
-    #     format_buffer = max_len
     return f"{prefix:<20} {team1_pets_str:>{format_buffer}}     VS     {team2_pets_str:<{format_buffer}}"
 
 
-def fight(team1, team2, verbose=True):
-    if verbose:
-        print("\n", "- "*10)
-        print(get_battle_string(team1, team2, prefix='Start of Battle :'))
+def fight(team1, team2):
+    log.debug("---------------------")
+    log.debug(get_battle_string(team1, team2, prefix='Start of Battle :'))
 
     # Start of Battle
     pet_list = []
@@ -49,40 +49,25 @@ def fight(team1, team2, verbose=True):
 
         round += 1
 
-        if verbose:
-            print(get_battle_string(team1, team2, prefix=f'Before Attack :'))
+        log.debug(get_battle_string(team1, team2, prefix=f'Before Attack :'))
 
         pet1.before_attack()
         pet2.before_attack()
 
-        if verbose:
-            print(get_battle_string(team1, team2, prefix=f'Round ({round}):'))
+        log.debug(get_battle_string(team1, team2, prefix=f'Round ({round}):'))
 
         pet1.attack_pet(pet2)
 
-        if verbose:
-            print(get_battle_string(team1, team2, prefix=f'After Fight :'))
-            print("\n")
+        log.debug(get_battle_string(team1, team2, prefix=f'After Fight :'))
 
-        # Double check that pet still exists, then remove if dead from combat
-        # if team1.pets:
-        #     if team1.pets[0] == pet1 and pet1.health <= 0:
-        #         team1.remove_pet(pet1)
-        # if team2.pets:
-        #     if team2.pets[0] == pet2 and pet2.health <= 0:
-        #         team2.remove_pet(pet2)
-
-        if verbose:
-            if not team1.pets:
-                print("Team 1 loses")
-            if not team2.pets:
-                print("Team 2 loses")
-            if not team1.pets and not team2.pets:
-                print("It's a tie!")
+        if not team1.pets:
+            log.debug("Team 1 loses")
+        if not team2.pets:
+            log.debug("Team 2 loses")
+        if not team1.pets and not team2.pets:
+            log.debug("It's a tie!")
     # End of Battle
-    if verbose:
-        print("End of Battle")
-        print(get_battle_string(team1, team2, prefix=f'Final Board State :'))
-        print("\n")
+    log.debug("End of Battle")
+    log.debug(get_battle_string(team1, team2, prefix=f'Final Board State :'))
 
     return [int(bool(team1.pets)), int(bool(team2.pets))]
--- src/main.py
+++ src/main.py
@@ -16,13 +16,13 @@ def create_random_team(pet_pool, team_size=5):
     return team
 
 
-def run_simulation(num_sims, friendly_pool, enemy_pool, verbose=False, friendly_team_size=5, enemy_team_size=5):
+def run_simulation(num_sims, friendly_pool, enemy_pool, friendly_team_size=5, enemy_team_size=5):
     total = [0, 0]
 
     for i in range(num_sims):
         friendly_team = create_random_team(friendly_pool, team_size=friendly_team_size)
         enemy_team = create_random_team(enemy_pool, team_size=enemy_team_size)
-        result = fight(friendly_team, enemy_team, verbose=verbose)
+        result = fight(friendly_team, enemy_team)
         total[0] += result[0]
         total[1] += result[1]
 
@@ -31,9 +31,8 @@ def run_simulation(num_sims, friendly_pool, enemy_pool, verbose=False, friendly_
 
 def main(sims, friendly_team_size=5, enemy_team_size=5, friendly_pool=pet_data_manager.TEST_POOL,
          enemy_pool=pet_data_manager.TEST_POOL2):
-    verbose = sims <= 5
 
-    total, num_sims = run_simulation(sims, friendly_pool, enemy_pool, verbose, friendly_team_size, enemy_team_size)
+    total, num_sims = run_simulation(sims, friendly_pool, enemy_pool, friendly_team_size, enemy_team_size)
 
     win_rate = total[0] / num_sims
     loss_rate = total[1] / num_sims
