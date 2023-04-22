510537a7bdfaba84b48563e451affaf03abb7c88
MrKnowles55 <97849647+MrKnowles55@users.noreply.github.com>
2023-04-21 21:00:18 -0400

    break apart fight() to start_of_battle, fight_loop and end_of_battle to help with ability layering. start_of_battle prioritizes pets, collects triggered abilities, then applies them in order of priority.

--- src/battle.py
+++ src/battle.py
@@ -1,4 +1,7 @@
-from pet import prioritize_pets
+from utils import prioritize_pets, collect_triggered_abilities
+from pet_data_utils.enums.trigger_event import TriggerEvent
+from pet_data_utils.enums.effect_kind import EffectKind
+from pet_data_utils.enums.effect_target_kind import EffectTargetKind
 import logger
 
 log = logger.setup_logger(__name__)
@@ -33,37 +36,41 @@ def get_pet_list(team1, team2):
     return team1.pets + team2.pets
 
 
-def fight(team1, team2, verbose):
-    """
-    Simulate a battle between two teams of pets.
-
-    :param team1: The first team of pets.
-    :param team2: The second team of pets.
-    :param verbose: Whether to print battle information.
-    :return: A list containing the result of the battle for each team (1 if the team won, 0 otherwise).
-    """
-    log.debug("Starting Fight")
-    log.debug(get_battle_string(team1, team2, prefix='Start of Battle :'))
-
-    if verbose:
-
-        print("\n")
-        print(get_battle_string(team1, team2, prefix='Start of Battle :'))
-
-    # Start of Battle
-    pet_list = get_pet_list(team1, team2)
-
+def start_of_battle(team1, team2, pet_list, verbose):
+    actions = []
     priority_dict = prioritize_pets(pet_list)
+    priorities = sorted(priority_dict.keys(), reverse=True)
 
-    for priority in sorted(priority_dict.keys(), reverse=True):
+    # Initialize applied damage dictionary
+    applied_damage = {}
+
+    for priority in priorities:
         pets_with_same_priority = priority_dict[priority]
 
+        # Collect abilities that should trigger for StartOfBattle event
+        triggered_abilities = []
         for pet in pets_with_same_priority:
             if pet.team == team1:
-                pet.start_of_battle(enemy_team=team2)
+                triggered_abilities += collect_triggered_abilities([pet], TriggerEvent.StartOfBattle, priority,
+                                                                   enemy_team=team2, applied_damage=applied_damage)
             else:
-                pet.start_of_battle(enemy_team=team1)
+                triggered_abilities += collect_triggered_abilities([pet], TriggerEvent.StartOfBattle, priority,
+                                                                   enemy_team=team1, applied_damage=applied_damage)
 
+        # Execute the collected abilities
+        for ability_priority, ability, enemy_team, applied_damage in triggered_abilities:
+            actions += ability.apply(ability.owner, ability.owner.team, enemy_team=enemy_team, applied_damage=applied_damage)
+
+        for action in actions:
+            action_name, *args = action
+            if action_name == "take_damage":
+                pet, damage, attacker = args
+                pet._apply_damage(damage, attacker)
+    if verbose:
+        print(get_battle_string(team1, team2, prefix="Start of Battle"))
+
+
+def fight_loop(team1, team2, pet_list, verbose=False):
     # Fight Loop
     round = 0
     while team1.pets and team2.pets:
@@ -92,9 +99,45 @@ def fight(team1, team2, verbose):
             log.debug("Team 2 loses")
         if not team1.pets and not team2.pets:
             log.debug("It's a tie!")
+
+
+def end_of_battle(team1, team2, pet_list, verbose=False):
     # End of Battle
     log.debug("End of Battle")
     log.debug(get_battle_string(team1, team2, prefix=f'Final Board State :'))
     log.debug("Finished Fight")
 
     return [int(bool(team1.pets)), int(bool(team2.pets))]
+
+
+def fight(team1, team2, verbose):
+    """
+    Simulate a battle between two teams of pets.
+
+    :param team1: The first team of pets.
+    :param team2: The second team of pets.
+    :param verbose: Whether to print battle information.
+    :return: A list containing the result of the battle for each team (1 if the team won, 0 otherwise).
+    """
+    log.debug("Starting Fight")
+    log.debug(get_battle_string(team1, team2, prefix='Start of Battle :'))
+
+    if verbose:
+
+        print("\n")
+        print(get_battle_string(team1, team2, prefix='Before Fight :'))
+
+    # Start of Battle
+    pet_list = get_pet_list(team1, team2)
+
+    priority_dict = prioritize_pets(pet_list)
+
+    start_of_battle(team1, team2, pet_list, verbose)
+    fight_loop(team1, team2, pet_list, verbose)
+
+    return end_of_battle(team1, team2, pet_list, verbose)
+
+
+
+
+
