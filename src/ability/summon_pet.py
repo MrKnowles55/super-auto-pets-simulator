0f5f1d8a72526d2d704f736bbe62b38419075dc4
MrKnowles55 <97849647+MrKnowles55@users.noreply.github.com>
2023-04-28 21:30:21 -0400

    changed SummonSpecific apply() method to return actions

--- src/ability/summon_pet.py
+++ src/ability/summon_pet.py
@@ -2,6 +2,7 @@ from abc import abstractmethod
 from src.ability.ability_abstract import AbilityBase
 import config_utils.logger as logger
 from src.pet_data_utils.enums.trigger_event import TriggerEvent
+from src.action.action_utils import *
 
 log = logger.setup_logger(__name__)
 # parent_dir = os.path.abspath(os.path.join(os.getcwd(), ".."))
@@ -26,8 +27,7 @@ class Summon(AbilityBase):
 
 class SummonSpecific(Summon):
     def apply(self, pet, team, **kwargs):
-        from pet.pet_factory import create_pet
-        my_team = self.owner.team
+        actions = []
         if self.trigger_event == TriggerEvent.Faint:
             if self.team_tag == "Friendly":
                 team_to_add_to = team
@@ -37,15 +37,20 @@ class SummonSpecific(Summon):
                 index = 0
 
             pets_created = 0
-            team_to_add_to.remove_pet(pet)
+            actions.append(generate_remove_action(self.owner, self.owner.team))
             while pets_created < self.n:
-                try:
-                    new_pet = create_pet(self.token)
-                    log.debug(f"{self.owner} {self.__class__.__name__} summoning {new_pet.name} to {team_to_add_to}")
-                    team_to_add_to.add_pet(new_pet, index)
-                except KeyError:
-                    log.debug(f"Cannot create pet of type {self.token}")
+                actions.append(generate_summon_action(self.token, team_to_add_to, index))
                 pets_created += 1
+
+            return actions
+            # while pets_created < self.n:
+            #     try:
+            #         new_pet = create_pet(self.token)
+            #         log.debug(f"{self.owner} {self.__class__.__name__} summoning {new_pet.name} to {team_to_add_to}")
+            #         team_to_add_to.add_pet(new_pet, index)
+            #     except KeyError:
+            #         log.debug(f"Cannot create pet of type {self.token}")
+            #     pets_created += 1
         else:
             print(f'{self.__class__}:{self.trigger_event} not implemented')
 
