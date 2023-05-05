3309674b003c18c74b29772378f7894aa6de546c
MrKnowles55 <97849647+MrKnowles55@users.noreply.github.com>
2023-05-05 11:28:39 -0400

    updated ot be an appropriate unit test of Generic Ability and No Ability

--- tests/unit/ability/test_ability.py
+++ tests/unit/ability/test_ability.py
@@ -1,70 +1,47 @@
 import unittest
-from src.pet.pet_entity import PetEntity
-from src.ability.ability_generator import AbilityGenerator
 from src.ability.ability_abstract import AbilityBase, No_Ability
-from src.team.team import Team
 from src.pet_data_utils.enums.trigger_event import TriggerEvent
+from tests.dummy.dummy_pet import DummyPet
+
+
+class Fake_Ability(AbilityBase):
+    def apply(self, **kwargs):
+        return self.trigger_event
 
 
 class TestAbility(unittest.TestCase):
-
     def setUp(self):
-        self.test_pet = PetEntity(name="Test Pet", attack=1, health=1, tier=1, level=1,
-                                  ability1=None, ability2=None, ability3=None, ability_generator=AbilityGenerator)
-        self.test_team = Team("Test")
-        self.test_team.add_pet(self.test_pet)
+        self.ability_none = No_Ability(owner=None)
+        self.ability_fake = Fake_Ability(owner=None, trigger_event=None)
 
-        # self.friend_pet1 = PetEntity(name="Test Friend Pet 1", attack=1, health=1, tier=1, level=1,
-        #                              ability1=None, ability2=None, ability3=None, ability_generator=AbilityGenerator)
-        # self.friend_pet2 = PetEntity(name="Test Friend Pet 2", attack=1, health=1, tier=1, level=1,
-        #                              ability1=None, ability2=None, ability3=None, ability_generator=AbilityGenerator)
+    def test_init(self):
+        # No Ability
+        self.assertFalse(self.ability_none.owner)
+        self.assertFalse(self.ability_none.trigger_event)
 
-        # self.test_team.add_pet(self.friend_pet1)
-        # self.test_team.add_pet(self.friend_pet2)
+        # Fake ability
+        fake_pet = DummyPet()
+        new_ability = Fake_Ability(owner=fake_pet, trigger_event=TriggerEvent.Hurt)
 
-    def test_no_ability_instance_and_trigger_event(self):
-        ability = No_Ability(self.test_pet)
+        self.assertEqual(new_ability.owner, fake_pet)
+        self.assertEqual(new_ability.trigger_event, TriggerEvent.Hurt)
 
-        ability.apply()
-        self.assertIsNone(ability.trigger_event)
-        self.assertIsInstance(ability, AbilityBase)
+    def test_apply(self):
+        # No ability
+        self.assertFalse(self.ability_none.apply())
 
+        # Fake Ability
+        self.ability_fake.trigger_event = TriggerEvent.Sell
+        self.assertEqual(self.ability_fake.apply(), TriggerEvent.Sell)
 
-class Test_AbilityBase:
-    def __init__(self, owner, trigger_event=None):
-        self.owner = owner
-        self.trigger_event = trigger_event
+    def test_trigger(self):
+        for event in TriggerEvent:
+            # No ability
+            self.assertFalse(self.ability_none.trigger(event))
 
-    def apply(self, **kwargs):
-        if self.trigger_event:
-            return self.trigger_event.name, kwargs
-
-        return "None", kwargs
-
-    def trigger(self, event, **kwargs):
-        if event == self.trigger_event:
-            return self.apply(**kwargs)
-
-
-class Test_NoAbility(Test_AbilityBase):
-    def __init__(self, owner):
-        super().__init__(owner)
-
-
-class Test_HurtAbility(Test_AbilityBase):
-    def __init__(self, owner, trigger_event=TriggerEvent.Hurt):
-        super().__init__(owner, trigger_event)
-
-
-def generate_test_ability(owner, trigger_event=None):
-    trigger_event_dict = {item.name.lower(): item for item in TriggerEvent}
-
-    if trigger_event and trigger_event.lower() in trigger_event_dict:
-        trigger_event = trigger_event_dict[trigger_event.lower()]
-    else:
-        trigger_event = None
-
-    return Test_AbilityBase(owner, trigger_event)
+            # Fake Ability
+            self.ability_fake.trigger_event = event
+            self.assertEqual(self.ability_fake.trigger(event), event)
 
 
 if __name__ == '__main__':
