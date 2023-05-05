e4cc1c9771e90f9257c1066d2c390dc63f4cf9bd
MrKnowles55 <97849647+MrKnowles55@users.noreply.github.com>
2023-05-05 13:14:08 -0400

    converted to proper unittest

--- tests/unit/team/test_teams.py
+++ tests/unit/team/test_teams.py
@@ -1,51 +1,192 @@
 import unittest
-from src.pet.pet_entity import PetEntity
-from src.ability.ability_generator import AbilityGenerator
+from tests.dummy.dummy_pet import generate_dummy_pet
+from tests.dummy.dummy_action import Dummy_ActionHandler
 from src.team.team import Team
 
 
 class TestTeam(unittest.TestCase):
 
-    def test_add_pet(self):
-        team = Team("Test")
-        pet = PetEntity("Test Pet", 5, 10, 1, 1, None, None, None, AbilityGenerator)
+    def setUp(self) -> None:
+        self.action_handler = Dummy_ActionHandler()
+        self.action_handler.clear_actions()
+        self.test_team = Team(name="Name", handler=self.action_handler)
 
-        team.add_pet(pet)
-        self.assertIn(pet, team.pets_list)
+    def test_add_pet(self):
+        pet = generate_dummy_pet(name="Pet 0")
+        pet1 = generate_dummy_pet(name="Pet 1")
+        pet2 = generate_dummy_pet(name="Pet 2")
+
+        # Without Index
+        self.test_team.pets_list = []
+        self.test_team.add_pet(pet)
+
+        self.assertIn(pet, self.test_team.pets_list)
+        self.assertEqual(pet.team, self.test_team)
+        self.assertEqual(self.test_team.length, 1)
+        self.assertEqual(pet.position, 0)
+
+        # With Index
+        self.test_team.pets_list = []
+        self.test_team.add_pet(pet1)
+        self.test_team.add_pet(pet2)
+        self.test_team.add_pet(pet, index=1)
+
+        self.assertEqual(self.test_team.pets_list[0].name, "Pet 1")
+        self.assertEqual(self.test_team.pets_list[1].name, "Pet 0")
+        self.assertEqual(self.test_team.pets_list[2].name, "Pet 2")
+        self.assertEqual(self.test_team.length, 3)
+
+        # Avoid adding too many pets
+        self.test_team.pets_list = []
+        for _ in range(6):
+            self.test_team.add_pet(pet)
+
+        self.assertEqual(self.test_team.length, 5)
 
     def test_remove_pet(self):
-        team = Team("Test")
-        pet = PetEntity("Test Pet", 5, 10, 1, 1, None, None, None, AbilityGenerator)
+        pet = generate_dummy_pet(name="Pet 0")
+        pet1 = generate_dummy_pet(name="Pet 1")
+        pet2 = generate_dummy_pet(name="Pet 2")
 
-        team.add_pet(pet)
-        team.remove_pet(pet)
-        self.assertNotIn(pet, team.pets_list)
+        # Remove from front
+        self.test_team.pets_list = []
+        self.test_team.pets_list.append(pet)
+
+        self.test_team.remove_pet(pet)
+
+        self.assertFalse(self.test_team.pets_list)
+        self.assertTrue(self.action_handler.action_list)
+
+        # Remove 1 from front with pets behind
+        self.action_handler.clear_actions()
+        self.test_team.pets_list = []
+        self.test_team.pets_list.append(pet)
+        self.test_team.pets_list.append(pet1)
+
+        self.test_team.remove_pet(pet)
+
+        self.assertNotIn(pet, self.test_team.pets_list)
+        self.assertIn(pet1, self.test_team.pets_list)
+        self.assertEqual(pet1.position, 0)
+
+        # Remove 1 pet from middle
+        self.action_handler.clear_actions()
+        self.test_team.pets_list = []
+        self.test_team.pets_list.append(pet)
+        self.test_team.pets_list.append(pet1)
+        self.test_team.pets_list.append(pet2)
+
+        self.test_team.remove_pet(pet1)
+
+        self.assertEqual(self.test_team.first, pet)
+        self.assertEqual(pet.position, 0)
+        self.assertEqual(self.test_team.pets_list[1], pet2)
+        self.assertEqual(pet2.position, 1)
 
     def test_move_pet(self):
-        team = Team("Test")
-        pet1 = PetEntity("Pet 1", 5, 10, 1, 1, None, None, None, AbilityGenerator)
-        pet2 = PetEntity("Pet 2", 6, 12, 1, 1, None, None, None, AbilityGenerator)
+        pet = generate_dummy_pet(name="Pet 0")
+        pet1 = generate_dummy_pet(name="Pet 1")
+        pet2 = generate_dummy_pet(name="Pet 2")
+        pet3 = generate_dummy_pet(name="Pet 3")
+        pet4 = generate_dummy_pet(name="Pet 4")
 
-        team.add_pet(pet1)
-        team.add_pet(pet2)
-        team.move_pet(0, 1)
+        # Move empty space
+        self.test_team.pets_list = []
+        self.test_team.pets_list.append(pet)
+        pet.position = 0
 
-        self.assertEqual(team.pets_list, [pet2, pet1])
+        self.test_team.move_pet(4, 0)
 
-    def test_add_pet_with_index(self):
-        team = Team("Test")
-        pet1 = PetEntity("Pet 1", 5, 10, 1, 1, None, None, None, AbilityGenerator)
-        pet2 = PetEntity("Pet 2", 6, 12, 1, 1, None, None, None, AbilityGenerator)
+        self.assertEqual(self.test_team.first, pet)
+        self.assertEqual(self.test_team.length, 1)
 
-        team.add_pet(pet1)
-        team.add_pet(pet2, 0)
+        # Move in place, 1 pet
+        self.test_team.pets_list = []
+        self.test_team.pets_list.append(pet)
+        pet.position = 0
 
-        self.assertEqual(team.pets_list, [pet2, pet1])
+        self.test_team.move_pet(0, 0)
 
+        self.assertEqual(self.test_team.first, pet)
+        self.assertEqual(pet.position, 0)
 
-# Mock Team class to be used in tests
-MockTeamA = Team("Test_A")
-MockTeamB = Team("Test_B")
+        # Move in place, full team
+        self.test_team.pets_list = []
+        self.test_team.pets_list.append(pet)
+        self.test_team.pets_list.append(pet1)
+        self.test_team.pets_list.append(pet2)
+        self.test_team.pets_list.append(pet3)
+        self.test_team.pets_list.append(pet4)
+        pet.position = 0
+        pet1.position = 1
+        pet2.position = 2
+        pet3.position = 3
+        pet4.position = 4
+
+        self.test_team.move_pet(0, 0)
+
+        self.assertEqual(self.test_team.first, pet)
+        self.assertEqual(pet.position, 0)
+        self.assertEqual(self.test_team.pets_list[1], pet1)
+        self.assertEqual(pet1.position, 1)
+        self.assertEqual(self.test_team.pets_list[2], pet2)
+        self.assertEqual(pet2.position, 2)
+        self.assertEqual(self.test_team.pets_list[3], pet3)
+        self.assertEqual(pet3.position, 3)
+        self.assertEqual(self.test_team.pets_list[4], pet4)
+        self.assertEqual(pet4.position, 4)
+
+        # Move to index > team length
+        self.test_team.pets_list = []
+        self.test_team.pets_list.append(pet)
+        pet.position = 0
+
+        self.test_team.move_pet(0, 4)
+
+        self.assertEqual(self.test_team.first, pet)
+        self.assertEqual(pet.position, 0)
+
+        # Move 0 to 1 with 2 pets
+        self.test_team.pets_list = []
+        self.test_team.pets_list.append(pet)
+        self.test_team.pets_list.append(pet1)
+        pet.position = 0
+        pet1.position = 1
+
+        self.test_team.move_pet(0, 1)
+
+        self.assertEqual(self.test_team.first, pet1)
+        self.assertEqual(pet1.position, 0)
+        self.assertEqual(self.test_team.pets_list[1], pet)
+        self.assertEqual(pet.position, 1)
+        self.assertEqual(self.test_team.length, 2)
+
+    def test_update_positions(self):
+        pet = generate_dummy_pet(name="Pet 0")
+        pet1 = generate_dummy_pet(name="Pet 1")
+        pet2 = generate_dummy_pet(name="Pet 2")
+        pet3 = generate_dummy_pet(name="Pet 3")
+        pet4 = generate_dummy_pet(name="Pet 4")
+        self.test_team.pets_list = []
+        self.test_team.pets_list.append(pet)
+        self.test_team.pets_list.append(pet1)
+        self.test_team.pets_list.append(pet2)
+        self.test_team.pets_list.append(pet3)
+        self.test_team.pets_list.append(pet4)
+        pet.position = 4
+        pet1.position = 2
+        pet2.position = 3
+        pet3.position = 0
+        pet4.position = 1
+
+        self.test_team.update_positions()
+
+        self.assertEqual(pet.position, 0)
+        self.assertEqual(pet1.position, 1)
+        self.assertEqual(pet2.position, 2)
+        self.assertEqual(pet3.position, 3)
+        self.assertEqual(pet4.position, 4)
+
 
 if __name__ == '__main__':
     unittest.main()
