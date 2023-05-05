3419fe324a97555fc7dffee57bbe751ec3313c51
MrKnowles55 <97849647+MrKnowles55@users.noreply.github.com>
2023-05-05 10:38:40 -0400

    added test ability and a generator function to be imported into tests cases that require abilities with specific triggers

--- tests/unit/ability/test_ability.py
+++ tests/unit/ability/test_ability.py
@@ -3,6 +3,7 @@ from src.pet.pet_entity import PetEntity
 from src.ability.ability_generator import AbilityGenerator
 from src.ability.ability_abstract import AbilityBase, No_Ability
 from src.team.team import Team
+from src.pet_data_utils.enums.trigger_event import TriggerEvent
 
 
 class TestAbility(unittest.TestCase):
@@ -27,6 +28,43 @@ class TestAbility(unittest.TestCase):
         ability.apply()
         self.assertIsNone(ability.trigger_event)
         self.assertIsInstance(ability, AbilityBase)
+
+
+class Test_AbilityBase:
+    def __init__(self, owner, trigger_event=None):
+        self.owner = owner
+        self.trigger_event = trigger_event
+
+    def apply(self, **kwargs):
+        if self.trigger_event:
+            return self.trigger_event.name, kwargs
+
+        return "None", kwargs
+
+    def trigger(self, event, **kwargs):
+        if event == self.trigger_event:
+            return self.apply(**kwargs)
+
+
+class Test_NoAbility(Test_AbilityBase):
+    def __init__(self, owner):
+        super().__init__(owner)
+
+
+class Test_HurtAbility(Test_AbilityBase):
+    def __init__(self, owner, trigger_event=TriggerEvent.Hurt):
+        super().__init__(owner, trigger_event)
+
+
+def generate_test_ability(owner, trigger_event=None):
+    trigger_event_dict = {item.name.lower(): item for item in TriggerEvent}
+
+    if trigger_event and trigger_event.lower() in trigger_event_dict:
+        trigger_event = trigger_event_dict[trigger_event.lower()]
+    else:
+        trigger_event = None
+
+    return Test_AbilityBase(owner, trigger_event)
 
 
 if __name__ == '__main__':
