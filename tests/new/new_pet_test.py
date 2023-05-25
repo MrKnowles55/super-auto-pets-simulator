3c708654b1add22b8d4309a419b8fcb7fecb63ac
MrKnowles55 <97849647+MrKnowles55@users.noreply.github.com>
2023-05-25 14:15:17 -0400

    base Pet test

---
+++ tests/new/new_pet_test.py
@@ -0,0 +1,94 @@
+import unittest
+from unittest.mock import MagicMock
+from pet.new_pet import Pet
+
+from src.pet_data_utils.enums.trigger_event import TriggerEvent
+from src.pet_data_utils.enums.trigger_by_kind import TriggerByKind
+from src.pet_data_utils.enums.effect_target_kind import EffectTargetKind
+from src.pet_data_utils.enums.effect_kind import EffectKind
+
+
+class TestAnt(unittest.TestCase):
+    def setUp(self):
+        self.pet = Pet("TestPet")
+        # self.friend_pet = Pet("FriendPet")
+        # self.enemy_pet = Pet("FriendPet")
+        print("\n")
+
+    def test_pet_init(self):
+        """
+        Test pet creation and initialization
+        :return:
+        """
+        expected_dict = {
+            "id": 0,
+            "name": "TestPet",
+            "base_attack": 1,
+            "base_health": 1,
+            "tier": 1,
+            "ability": {
+                   "trigger": TriggerEvent.Test,
+                   "triggered_by": TriggerByKind.Test,
+                   "effect": EffectKind.Test,
+                   "effect_dict": {"target": EffectTargetKind.Test}
+               },
+            "level": 1,
+            "attack_mod": 0,
+            "health_mod": 0,
+            "team": None,
+            "start_position": -1,
+            "position": -1,
+            "attack": 1,
+            "health": 1
+        }
+
+        for key, value in expected_dict.items():
+            self.assertEqual(self.pet.__dict__[key], value)
+
+        # Properties
+        self.assertTrue(self.pet.alive)
+        self.assertEqual(self.pet.trigger, self.pet.ability.get("trigger"))
+        self.assertEqual(self.pet.triggered_by, self.pet.ability.get("triggered_by"))
+
+    def test_get_relationship(self):
+        self.pet.team = MagicMock()
+        self.pet.team.pets_list = [self.pet]
+
+        # Self
+        self.assertEqual(TriggerByKind.Self, self.pet.get_relationship(self.pet))
+
+        # Enemy
+        enemy_pet = Pet("EnemyPet")
+        enemy_pet.team = MagicMock()
+        enemy_pet.team.pets_list = [enemy_pet]
+
+        self.assertEqual(TriggerByKind.EachEnemy, self.pet.get_relationship(enemy_pet))
+        self.assertEqual(TriggerByKind.EachEnemy, enemy_pet.get_relationship(self.pet))
+
+        # EachFriend & FriendAhead
+        friend_pet = Pet("FriendPet")
+        friend_pet.team = self.pet.team
+        friend_pet.team.pets_list.append(friend_pet)
+
+        # Friend behind test pet should see FriendAhead, and test pet sees EachFriend
+        self.pet.position = 0
+        friend_pet.position = 1
+
+        self.assertEqual(TriggerByKind.EachFriend, self.pet.get_relationship(friend_pet))
+        self.assertEqual(TriggerByKind.FriendAhead, friend_pet.get_relationship(self.pet))
+
+        # Now the opposite
+        self.pet.position = 1
+        friend_pet.position = 0
+
+        self.assertEqual(TriggerByKind.FriendAhead, self.pet.get_relationship(friend_pet))
+        self.assertEqual(TriggerByKind.EachFriend, friend_pet.get_relationship(self.pet))
+
+        # Player
+        self.assertEqual(TriggerByKind.Player, self.pet.get_relationship(MagicMock()))
+
+
+
+
+
+
