c87902f9673c74de9d5d86c60b32a5e620359861
MrKnowles55 <97849647+MrKnowles55@users.noreply.github.com>
2023-06-13 20:29:01 -0400

    test_new_pet

---
+++ tests/test_new_pet.py
@@ -0,0 +1,29 @@
+import logging
+import unittest
+from unittest.mock import MagicMock, patch
+
+from src.pet_utils.pet import Pet
+from src.action_utils.battle import Battle, Action, PriorityQueue
+from src.team_utils.team import Team
+from src.action_utils.signals import send_signal, Signal
+from src.config_utils.custom_logger import setup_logging
+from src.data_utils.ability_enums import EffectKind, EffectTargetKind, TriggerByKind, TriggerEvent
+
+setup_logging(logging.DEBUG)
+
+
+class TestPet(unittest.TestCase):
+    def setUp(self) -> None:
+        self.player_team = Team("Player")
+        self.enemy_team = Team("Enemy")
+        self.battle = Battle(self.player_team, self.enemy_team)
+        print("\n")
+
+        self.pet = Pet("Mosquito")
+        self.player_team.add_pet(self.pet)
+
+    def test(self):
+        self.enemy_team.add_pet(Pet("Test"))
+
+        results = self.battle.battle_loop()
+
